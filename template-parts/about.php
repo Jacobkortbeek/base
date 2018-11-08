15b1a8eed48a5ab489eb2648360b4c595c34158e
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-11-08 22:34:58 +0000

    about partial

--- template-parts/about.php
+++ template-parts/about.php
@@ -1,13 +1,13 @@
 <div class="container about">
         <div class="row">
           <div class="col-md-6 content">
-            <h3><?php the_field( 'title' ); ?></h3>
+            <h3><?php the_field( 'title', 23 ); ?></h3>
             <p>
-              <?php the_field( 'paragraph' ); ?>
+              <?php the_field( 'paragraph', 23 ); ?>
             </p>
           </div>
           <div class="col-md-6 image">
-            <?php $about_image = get_field( 'about_image' ); ?>
+            <?php $about_image = get_field( 'about_image', 23 ); ?>
             <?php if ( $about_image ) { ?>
 	             <img src="<?php echo $about_image['url']; ?>" alt="<?php echo $about_image['alt']; ?>" />
             <?php } ?>
@@ -15,7 +15,7 @@
         </div>
         <div class="row">
           <div class="col-md-12">
-            <?php the_field( 'about_button' ); ?>
+            <?php the_field( 'about_button', 23 ); ?>
             <button type="button" name="button">More info</button>
           </div>
         </div>
