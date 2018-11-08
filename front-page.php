80c3a42ac417d9237394f2fe4480ed35cb970d76
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-11-08 22:06:52 +0000

    hero

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
   <main class="home">
     <div class="container-fluid hero" style="<?php if ( get_field( 'background_image') ) { ?>
-	                                             background-image: <?php the_field( 'background_image' ); ?> ;
+	                                             background-image: url('<?php the_field( 'background_image' ); ?>');
                                               <?php } ?>">
         <div class="row">
           <div class="col-md-12 logo">
