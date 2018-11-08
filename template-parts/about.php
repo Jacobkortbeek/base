f941eb1ac3e20edb65da0a29bd2113e3c1d7ddcb
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-11-08 22:25:56 +0000

    about partial

--- template-parts/about.php
+++ template-parts/about.php
@@ -15,6 +15,7 @@
         </div>
         <div class="row">
           <div class="col-md-12">
+            <?php the_field( 'about_button' ); ?>
             <button type="button" name="button">More info</button>
           </div>
         </div>
