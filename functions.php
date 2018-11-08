a99f6644cf278712eddf6f61b7171dad32d78487
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-11-08 21:27:04 +0000

    menu

--- functions.php
+++ functions.php
@@ -1,14 +1,38 @@
 <?php
 
-function wpt_theme_styles() {
-    wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap/bootstrap.min.css' );
-    wp_enqueue_style('font-awesome', '//use.fontawesome.com/releases/v5.5.0/css/all.css'); 
-  }
-  add_action( 'wp_enqueue_scripts', 'wpt_theme_styles' );
+  add_theme_support( 'menus' );
 
-function wpt_theme_js() {
-    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap/bootstrap.min.js', array('jquery'), ' ', true);
+  function register_theme_menus() {
+    register_nav_menus(
+      array(
+        'primary-menu' => __( 'Primary Menu' )
+      )
+    );
   }
-  add_action( 'wp_enqueue_scripts', 'wpt_theme_js' );
+  add_action( 'init', 'register_theme_menus' );
+
+  if( function_exists('acf_add_options_page') ) {
+
+      acf_add_options_page(array(
+  		'page_title' 	=> 'Theme General Settings',
+  		'menu_title'	=> 'Theme Settings',
+  		'menu_slug' 	=> 'theme-general-settings',
+  		'capability'	=> 'edit_posts',
+  		'redirect'		=> false
+  	));
+
+  }
+
+  function wpt_theme_styles() {
+      wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
+      wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap/bootstrap.min.css' );
+      wp_enqueue_style('font-awesome', '//use.fontawesome.com/releases/v5.5.0/css/all.css');
+    }
+    add_action( 'wp_enqueue_scripts', 'wpt_theme_styles' );
+
+  function wpt_theme_js() {
+      wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap/bootstrap.min.js', array('jquery'), ' ', true);
+    }
+    add_action( 'wp_enqueue_scripts', 'wpt_theme_js' );
+
 ?>
