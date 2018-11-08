4aaead5fed42852ce93c8d9e2b0c3f9ddb6ef8fc
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-11-08 22:00:08 +0000

    hero

--- front-page.php
+++ front-page.php
@@ -1,3 +1,20 @@
 <?php get_header(); ?>
-front page
+  <main class="home">
+    <div class="container-fluid hero" style="<?php if ( get_field( 'background_image') ) { ?>
+	                                             background-image: linear-gradient(
+                                                 rgba(250, 250, 250, 0.25),
+                                                 rgba(250, 250, 250, 0.25)
+                                                ), <?php the_field( 'background_image' ); ?>
+                                              <?php } ?>">
+        <div class="row">
+          <div class="col-md-12 logo">
+            <?php $logo-hero = get_field( 'logo-hero' ); ?>
+            <?php if ( $logo-hero ) { ?>
+	             <img src="<?php echo $logo-hero['url']; ?>" alt="<?php echo $logo-hero['alt']; ?>" />
+            <?php } ?>
+            <?php the_field( 'company-name' ); ?>
+          </div>
+        </div>
+      </div>
+  </main>
 <?php get_footer(); ?>
