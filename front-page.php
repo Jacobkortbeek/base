e3b6a6d93f7acba57fd6c6bab37fc82c43eb8ce2
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-11-08 23:03:05 +0000

    Slider

--- front-page.php
+++ front-page.php
@@ -14,7 +14,50 @@
         </div>
       </div>
 
+
       <?php get_template_part( 'template-parts/about', 'none' ); ?>
+
+      <div class="container gallery">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+      <?php
+      // check if the repeater field has rows of data
+      if( have_rows('gallery_slider') ):
+
+       	// loop through the rows of data
+          while ( have_rows('gallery_slider') ) : the_row(); ?>
+
+
+
+              <div class="carousel-item active">
+                <?php
+                // display a sub field value
+                the_sub_field('slider_image'); ?>
+              </div>
+
+        <?php  endwhile;
+
+      else :
+
+          // no rows found
+
+      endif;
+      ?>
+
+
+
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+      </div>
+
 
   </main>
 <?php get_footer(); ?>
