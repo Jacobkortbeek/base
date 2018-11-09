67b653008bb5b69e9554e6ae39208acdd9a26833
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-11-09 01:34:00 +0000

    portfolio

--- front-page.php
+++ front-page.php
@@ -57,42 +57,7 @@
 </div>
       </div>
 
-      <?php
-
-        $num_posts = get_option( 'posts_per_page' );
-
-        $args = array(
-          'post_type' => 'portfolio',
-          'posts_per_page' => $num_posts,
-          'orderby' => 'post_date'
-        );
-
-        $query = new WP_Query( $args );
-
-      ?>
-
-      <div class="container portfolio">
-        <div class="row port-row">
-          <?php $i=0; if( $query->have_posts() && $i < 3 ) : while( $query->have_posts() ) : $query->the_post(); ?>
-
-          <div class="col-md-6 port-item">
-            <div class="port" style="background-image: url('<?php if ( get_field( 'background_image') ) { ?>
-      	                                                   <?php the_field( 'background_image' ); ?>
-                                                           <?php } ?>');">
-              <div class="cont">
-                <h4><?php the_field( 'portfolio_title' ); ?></h4>
-                <p>
-                  <?php the_field( 'portfolio_info' ); ?>
-                </p>
-              </div>
-            </div>
-          </div>
-          <?php $i++;?>
-
-          <?php endwhile; endif; wp_reset_postdata(); ?>
-
-        </div>
-      </div>
+    <?php get_template_part( 'template-parts/portfolio', 'none' ); ?>
 
   </main>
 <?php get_footer(); ?>
