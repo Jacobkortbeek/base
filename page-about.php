6e2366e53db3cfae70151e641c24ee8bd1b7890f
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-11-09 20:33:32 +0000

    staff slider

--- page-about.php
+++ page-about.php
@@ -71,7 +71,7 @@
            	// loop through the rows of data
               while ( have_rows('collapsible_information', 23) ) : the_row(); ?>
         <div class="card">
-          <div class="card-header" id="headingOne">
+          <div class="card-header">
             <h5 class="mb-0">
               <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                 <?php // display a sub field value
@@ -80,7 +80,7 @@
             </h5>
           </div>
 
-          <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+          <div class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
             <div class="card-body">
               <?php the_sub_field('collapse_info'); ?>
             </div>
