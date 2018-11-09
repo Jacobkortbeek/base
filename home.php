7e3ae21721e5cb8e4c29d477ccd1a5afd52bd32f
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-11-09 21:25:52 +0000

    blog page

--- home.php
+++ home.php
@@ -1,40 +1,42 @@
 <?php get_header(); ?>
 
-<?php
+<main class="blog">
+  <?php
 
-  $num_posts = get_option( 'posts_per_page' );
+    $num_posts = get_option( 'posts_per_page' );
 
-  $args = array(
-    'post_type' => 'post',
-    'posts_per_page' => $num_posts,
-    'orderby' => 'post_date'
-  );
+    $args = array(
+      'post_type' => 'post',
+      'posts_per_page' => $num_posts,
+      'orderby' => 'post_date'
+    );
 
-  $query = new WP_Query( $args );
+    $query = new WP_Query( $args );
 
-?>
+  ?>
 
-<div class="container">
-    <?php $i=0; if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
-        <a href="<?php the_permalink(); ?>">
-        <div class="row blog-indv">
-          <div class="col-md-8">
-            <div class="cont">
-              <h2><?php the_title(); ?></h2>
-                <?php the_excerpt(); ?>
+  <div class="container">
+      <?php $i=0; if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
+          <a href="<?php the_permalink(); ?>">
+          <div class="row blog-indv">
+            <div class="col-md-8">
+              <div class="cont">
+                <h2><?php the_title(); ?></h2>
+                  <?php the_excerpt(); ?>
+              </div>
             </div>
-          </div>
-          <div class="col-md-4">
-            <div class="img">
-              <?php the_post_thumbnail( 'medium' ); ?>
+            <div class="col-md-4">
+              <div class="img">
+                <?php the_post_thumbnail( 'medium' ); ?>
+              </div>
             </div>
+            <hr>
           </div>
-          <hr>
+          </a>
+          <?php endwhile; endif; ?>
+
+
         </div>
-        </a>
-        <?php endwhile; endif; ?>
-
-
-      </div>
+</main>
 
 <?php get_footer(); ?>
