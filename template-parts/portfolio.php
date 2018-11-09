22b8de20500bdd51192030b0ad79663acf3c80a6
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-11-09 01:18:08 +0000

    portfolio

--- template-parts/portfolio.php
+++ template-parts/portfolio.php
@@ -3,7 +3,7 @@
   $num_posts = get_option( 'posts_per_page' );
 
   $args = array(
-    'numberposts' => $numPosts,
+    'numberposts' => 4,
     'post_type' => 'portfolio',
     'posts_per_page' => $num_posts,
     'orderby' => 'post_date'
@@ -16,7 +16,7 @@
 <div class="container portfolio">
   <div class="row port-row">
     <?php $i=0; if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
-
+      <?php if( $query->is_home() && $i == 3 ) : ?>
     <div class="col-md-6 port-item">
       <div class="port" style="background-image: url('<?php if ( get_field( 'background_image') ) { ?>
 	                                                   <?php the_field( 'background_image' ); ?>
@@ -29,6 +29,21 @@
         </div>
       </div>
     </div>
+  <?php else : ?>
+    <div class="col-md-6 port-item">
+      <div class="port" style="background-image: url('<?php if ( get_field( 'background_image') ) { ?>
+	                                                   <?php the_field( 'background_image' ); ?>
+                                                     <?php } ?>');">
+        <div class="cont">
+          <h4><?php the_field( 'portfolio_title' ); ?></h4>
+          <p>
+            <?php the_field( 'portfolio_info' ); ?>
+          </p>
+        </div>
+      </div>
+    </div>
+
+  <?php endif; ?>
     <?php $i++;?>
 
     <?php endwhile; endif; wp_reset_postdata(); ?>
