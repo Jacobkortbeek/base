59c3ba5cc28ea87ac8a83c108febe8722b04ffbe
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-11-09 19:53:07 +0000

    staff slider

--- functions.php
+++ functions.php
@@ -60,8 +60,8 @@ add_action( 'init', 'create_posttype' );
   function wpt_theme_styles() {
       wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
       wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap/bootstrap.min.css' );
-      wp_enqueue_style('owlCarouselCss', '/js/owlcarousel/assets/owl.carousel.min.css');
-      wp_enqueue_style('owlCarouselTheme', '/js/owlcarousel/assets/owl.theme.default.min.css');
+      wp_enqueue_style('owlCarouselCss', get_template_directory_uri() . '/js/owlcarousel/assets/owl.carousel.min.css');
+      wp_enqueue_style('owlCarouselTheme', get_template_directory_uri() . '/js/owlcarousel/assets/owl.theme.default.min.css');
       wp_enqueue_style('font-awesome', '//use.fontawesome.com/releases/v5.5.0/css/all.css');
     }
     add_action( 'wp_enqueue_scripts', 'wpt_theme_styles' );
