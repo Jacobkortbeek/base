5ea1fed44f7e1623733788d2bdd04b6486894939
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-11-09 20:34:37 +0000

    staff slider

--- page-about.php
+++ page-about.php
@@ -73,14 +73,14 @@
         <div class="card">
           <div class="card-header">
             <h5 class="mb-0">
-              <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true">
+              <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                 <?php // display a sub field value
                 the_sub_field('collapse_title'); ?>
               </button>
             </h5>
           </div>
 
-          <div class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+          <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
             <div class="card-body">
               <?php the_sub_field('collapse_info'); ?>
             </div>
