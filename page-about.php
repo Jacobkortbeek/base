1d7b4070d8e75ac0e91e525aedc5a2a27a05176e
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-11-09 19:50:41 +0000

    staff slider

--- page-about.php
+++ page-about.php
@@ -7,5 +7,51 @@
 <?php get_header(); ?>
 <main class="about">
   <?php get_template_part( 'template-parts/about', 'none' ); ?>
+
+
+
+
+
+  <div class="container staff-slider">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="owl-carousel loop">
+
+            <?php
+
+              $num_posts = get_option( 'posts_per_page' );
+
+              $args = array(
+                'post_type' => 'staff',
+                'posts_per_page' => $num_posts,
+                'orderby' => 'post_date'
+              );
+
+              $query = new WP_Query( $args );
+
+            ?>
+            <?php $i=0; if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
+
+            <div class="item">
+              <div class="profile-pic">
+                <?php $head_shot = get_field( 'head_shot' ); ?>
+                  <?php if ( $head_shot ) { ?>
+                  	<img src="<?php echo $head_shot['url']; ?>" alt="<?php echo $head_shot['alt']; ?>" />
+                  <?php } ?>
+              </div>
+              <div class="profile-cont">
+                <h5><?php the_field( 'full_name' ); ?></h5>
+                <p>
+                  <?php the_field( 'job_title' ); ?>
+                </p>
+              </div>
+            </div>
+
+            <?php endwhile; endif; ?>
+          </div>
+        </div>
+      </div>
+    </div>
+
 </main>
 <?php get_footer(); ?>
