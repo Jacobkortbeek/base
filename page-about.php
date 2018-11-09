c53b19d5f83f9e08f62d6687f7e7fb2b2ebfee64
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-11-09 20:27:42 +0000

    staff slider

--- page-about.php
+++ page-about.php
@@ -66,23 +66,23 @@
               <?php
 
           // check if the repeater field has rows of data
-          if( have_rows('collapsible_information') ):
+          if( have_rows('collapsible_information', 23) ):
 
            	// loop through the rows of data
-              while ( have_rows('collapsible_information') ) : the_row(); ?>
+              while ( have_rows('collapsible_information', 23) ) : the_row(); ?>
         <div class="card">
           <div class="card-header" id="headingOne">
             <h5 class="mb-0">
               <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                 <?php // display a sub field value
-                the_sub_field('collapse_title'); ?>
+                the_sub_field('collapse_title', 23); ?>
               </button>
             </h5>
           </div>
 
           <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
             <div class="card-body">
-              <?php the_sub_field('collapse_info'); ?>
+              <?php the_sub_field('collapse_info', 23); ?>
             </div>
           </div>
         </div>
