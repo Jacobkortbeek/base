1494aab89d279404b6318fce9985d16ca3f1d097
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-11-09 19:34:47 +0000

    contact

--- template-parts/contact.php
+++ template-parts/contact.php
@@ -3,31 +3,31 @@
           <div class="col-md-6">
             <div class="item">
               <i class="fa fa-envelope" aria-hidden="true"></i>
-              <h5><?php the_field( 'email' ); ?></h5>
+              <h5><?php the_field( 'email', 18 ); ?></h5>
             </div>
           </div>
           <div class="col-md-6">
             <div class="item">
               <i class="fa fa-mobile" aria-hidden="true"></i>
-              <h5><?php the_field( 'mobile' ); ?></h5>
+              <h5><?php the_field( 'mobile', 18 ); ?></h5>
             </div>
           </div>
           <div class="col-md-6">
             <div class="item">
               <i class="fa fa-phone" aria-hidden="true"></i>
-              <h5><?php the_field( 'phone' ); ?></h5>
+              <h5><?php the_field( 'phone', 18 ); ?></h5>
             </div>
           </div>
           <div class="col-md-6">
             <div class="item">
               <i class="fa fa-clock" aria-hidden="true"></i>
-              <h5><?php the_field( 'opening_times' ); ?></h5>
+              <h5><?php the_field( 'opening_times', 18 ); ?></h5>
             </div>
           </div>
         </div>
         <div class="row form">
           <div class="col-md-12">
-            <?php the_field( 'contact_form' ); ?>
+            <?php the_field( 'contact_form', 18 ); ?>
             <form class="" action="index.html" method="post">
               <input type="text" name="" value="" placeholder="name">
               <input type="text" name="" value="" placeholder="email">
