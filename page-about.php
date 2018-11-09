b9fac56aa4d136e31f754607331d1887f8f3e8fc
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-11-09 20:22:15 +0000

    staff slider

--- page-about.php
+++ page-about.php
@@ -53,5 +53,51 @@
       </div>
     </div>
 
-</main>
+
+
+
+
+
+
+<div class="container accordion">
+        <div class="row">
+          <div class="col-md-12">
+            <div id="accordion">
+              <?php
+
+          // check if the repeater field has rows of data
+          if( have_rows('collapsible_information') ):
+
+           	// loop through the rows of data
+              while ( have_rows('collapsible_information') ) : the_row(); ?>
+        <div class="card">
+          <div class="card-header" id="headingOne">
+            <h5 class="mb-0">
+              <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                <?php // display a sub field value
+                the_sub_field('collapse_title'); ?>
+              </button>
+            </h5>
+          </div>
+
+          <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+            <div class="card-body">
+              <?php the_sub_field('collapse_info'); ?>
+            </div>
+          </div>
+        </div>
+      <?php endwhile;
+
+  else :
+
+      // no rows found
+
+  endif;
+
+  ?>
+      </div>
+          </div>
+        </div>
+      </div>
+  </main>
 <?php get_footer(); ?>
