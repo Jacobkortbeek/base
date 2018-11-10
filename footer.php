d2f7510af7380e8aeece4bd2ef7343650f3ed05a
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-11-10 16:31:43 +0000

    footer

--- footer.php
+++ footer.php
@@ -2,40 +2,15 @@
     <hr>
     <div class="container-fluid">
       <div class="row">
-        <div class="col-md-4">
-          <h3>Title</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </div>
-        <div class="col-md-4 logo">
-          <img src="assets/images/logo.png" alt="">
-        </div>
-        <div class="col-md-4">
-          <ul class="nav flex-column">
-<li class="nav-item">
-  <a class="nav-link active" href="index.html">Home</a>
-</li>
-<li class="nav-item">
-  <a class="nav-link" href="about.html">About</a>
-</li>
-<li class="nav-item">
-  <a class="nav-link" href="portfolio.html">Portfolio</a>
-</li>
-<li class="nav-item">
-  <a class="nav-link" href="blog.html">Blog</a>
-</li>
-<li class="nav-item">
-  <a class="nav-link" href="gallery.html">Gallery</a>
-</li>
-<li class="nav-item">
-  <a class="nav-link" href="contact.html">Contact</a>
-</li>
-<li class="nav-item">
-  <a class="nav-link disabled" href="#">Disabled</a>
-</li>
-</ul>
-        </div>
+        <?php if( !dynamic_sidebar( 'footer-text' )) ?>
+        no sidebar text
+      <?php endif; ?>
+      <?php if( !dynamic_sidebar( 'footer-logo' )) ?>
+      no sidebar logo
+    <?php endif; ?>
+    <?php if( !dynamic_sidebar( 'footer-menu' )) ?>
+    no sidebar menu
+  <?php endif; ?>
       </div>
     </div>
     <?php wp_footer(); ?>
