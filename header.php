a9b42e8146ddf7c288500b0a1cbd720c0f52b37f
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-11-10 14:44:46 +0000

    blog-indv

--- header.php
+++ header.php
@@ -17,11 +17,21 @@
         <div class="row nav">
           <div class="col-md-12">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="index.html">BASE</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
+    <?php
+
+  				      $defaults = array(
+  				        'container' => 'ul',
+  				        'theme_location' => 'primary-menu',
+  				        'menu_class' => 'links'
+  				      );
+
+  				      wp_nav_menu( $defaults );
+
+  				    ?>
     <ul class="navbar-nav">
       <li class="nav-item active">
         <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
