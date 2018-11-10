5ef557538f52527783e3d204d1d40c38d49e6d41
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-11-10 16:32:21 +0000

    footer

--- footer.php
+++ footer.php
@@ -2,13 +2,13 @@
     <hr>
     <div class="container-fluid">
       <div class="row">
-        <?php if( !dynamic_sidebar( 'footer-text' )) ?>
+        <?php if( !dynamic_sidebar( 'footer-text' )) : ?>
         no sidebar text
       <?php endif; ?>
-      <?php if( !dynamic_sidebar( 'footer-logo' )) ?>
+      <?php if( !dynamic_sidebar( 'footer-logo' )) : ?>
       no sidebar logo
     <?php endif; ?>
-    <?php if( !dynamic_sidebar( 'footer-menu' )) ?>
+    <?php if( !dynamic_sidebar( 'footer-menu' )) : ?>
     no sidebar menu
   <?php endif; ?>
       </div>
