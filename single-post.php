33025ebb870912b5ab9bca4c412256a69174070b
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-11-10 14:34:25 +0000

    blog-indv

--- single-post.php
+++ single-post.php
@@ -3,7 +3,7 @@
     <?php
        // Start the loop.
        while ( have_posts() ) : the_post(); ?>
-    <div class="container-fluid title" style="background-image: linear-gradient( rgba(250, 250, 250, 0.25), rgba(250, 250, 250, 0.25) ), url('<?php the_post_thumbnail( 'full' ); ?>');">
+    <div class="container-fluid title" style="background-image: linear-gradient( rgba(250, 250, 250, 0.25), rgba(250, 250, 250, 0.25) ), url('<?php the_post_thumbnail_url( 'full' ); ?>');">
         <div class="row">
           <div class="col-md-12 text">
             <h1><?php the_title(); ?></h1>
