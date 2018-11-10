80141ff9aef9796546e208287159d4620da52728
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-11-10 15:53:26 +0000

    nav

--- functions.php
+++ functions.php
@@ -3,6 +3,15 @@
   add_theme_support( 'menus' );
   add_theme_support( 'post-thumbnails' );
 
+  add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+
+    function special_nav_class ($classes, $item) {
+      if (in_array('current-menu-item', $classes) ){
+        $classes[] = 'active ';
+      }
+    return $classes;
+  }
+
   function create_posttype() {
 
     register_post_type( 'staff',
--- js/functions.js
+++ js/functions.js
@@ -50,9 +50,6 @@ jQuery(document).ready(function($) {
 
 	$(document).ready(function() {
 		$('header nav li').addClass('nav-item');
-		$('header nav li').click(() => {
-			$(this).addClass('active');
-		});
 		$('header nav li a').addClass('nav-link');
 		console.log("added class");
 	});
